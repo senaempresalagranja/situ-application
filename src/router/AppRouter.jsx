b00c32441192b6eba0e4a608cb7888126414a48d
Daniel Cardenas Lozano <dcardenasl@sena.edu.co>
2024-04-25 15:48:35 -0500

    configuracion de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Router, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
 
 export const AppRouter = () => {
+  // verificacion de autenticacion del usuario
+  const authStatus = "not-authenticated";
   return (
     <Routes>
-      <Router></Router>
-      <Router></Router>
+      {authStatus === "not-authenticated" ? (
+        // creamos un fragmento
+        <>
+          (<Route path="/auth/*" element={<LoginPage />} />)
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
+      ) : (
+        <>
+          (<Route path="/" element={<CalendarPage />} />)
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
+      )}
     </Routes>
   );
 };
