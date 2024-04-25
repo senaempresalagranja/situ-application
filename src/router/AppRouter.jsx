8d8d1e9256675810ca61bfb54bcecd3d251ef8d1
senaempresalagranja <sena.empresa2012@gmail.com>
2024-04-25 09:48:21 -0500

    inicializacion de proyecto

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Router, Routes } from "react-router-dom";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Router></Router>
+      <Router></Router>
+    </Routes>
+  );
+};
