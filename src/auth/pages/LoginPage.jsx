208befb52ee16c5f0b8641a90e14aa46871cf9a9
senaempresalagranja <sena.empresa2012@gmail.com>
2024-04-29 04:43:10 -0500

    formulario de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,100 +1,80 @@
+import "../styles/auth.css";
 import logoSenaEmpresa from "../../assets/images/logoSenaEmpresa.png";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
 
 export const LoginPage = () => {
   return (
-    <div className="container d-flex flex-column">
-      <div
-        className="row align-items-center justify-content-center g-0
-        min-vh-100"
-      >
-        <nav className="nav">
-          <div className="nav-logo">
-            <p>INSITU</p>
-          </div>
-          <div className="nav-menu" id="navMenu">
-            <ul>
-              <li>
-                <Link href="#" className="link">
-                  Usuario Invitado
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="nav-menu-btn">
-            <i className="bx bx-menu" onclick="myMenuFunction()"></i>
-          </div>
-        </nav>
-        <div className="col-12 col-md-8 col-lg-6 col-xxl-4 py-8 py-xl-0">
-          <div className="card smooth-shadow-md">
-            <div className="card-body p-6">
-              <div className="mb-4 text-center">
-                <img
-                  src={logoSenaEmpresa}
-                  className="mb-2"
-                  alt=""
-                  width={100}
-                />
-
-                <h1 className="h1 text-center">Inicio de Sesion</h1>
-                <p className="text-center">¡Hola Lider de Talento Humano!</p>
-                <p className="text-center">
-                  Por favor ingresa tus credenciales para programar los turnos
-                  de esta semana.
-                </p>
+    <>
+      <header>
+        <div className="container">
+          <ul>
+            <li className="logo">
+              <div className="images">
+                <img src={logoSenaEmpresa} alt="" />
               </div>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Correo Electronico
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    name="email"
-                    placeholder="Ingresa tu correo electronico"
-                    required=""
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-control"
-                    name="password"
-                    placeholder="Ingresa tu contraseña"
-                    required=""
-                  />
-                </div>
-
-                <div>
-                  <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">
-                      Iniciar Sesion
-                    </button>
-                  </div>
-                  <div className="d-md-flex text-center mt-4">
-                    <div>
-                      <Link
-                        to={"change-password"}
-                        className="text-inherit
-                        fs-5"
-                      >
-                        ¿Olvidaste Tu Contraseña?
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+              <h2>
+                INSITU<span>.</span>
+              </h2>
+            </li>
+            <li>
+              <Link className="nav-link">Home</Link>
+            </li>
+            <li>
+              <Link className="nav-link">Join</Link>
+            </li>
+          </ul>
         </div>
-      </div>
-    </div>
+      </header>
+      <main>
+        <section className="contact">
+          <div className="container">
+            <div className="left">
+              <div className="form-wrapper">
+                <div className="contact-heading">
+                  <h1>
+                    Inicio de Sesion <span>.</span>
+                  </h1>
+                  <p className="text">
+                    ¡Bienvenida Lider de Talento Humano! Ingresa tus
+                    credenciales para gestionar los turnos rutinarios de esta
+                    semana
+                  </p>
+                </div>
+                <form action="" className="contact-form">
+                  <div className="input-wrap w-100">
+                    <input
+                      placeholder="Ingresa tu correo electronico"
+                      type="text"
+                      className="contact-input"
+                      autoComplete="off"
+                      name="email"
+                      required={true}
+                    />
+                    <label htmlFor="email">Correo Electronico</label>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                  </div>
+                  <div className="input-wrap w-100">
+                    <input
+                      placeholder="Ingresa tu contraseña"
+                      type="password"
+                      className="contact-input"
+                      autoComplete="off"
+                      name="password"
+                      required={true}
+                    />
+                    <label htmlFor="email">Contraseña</label>
+                    <FontAwesomeIcon icon={faKey} />
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/auth.css";
 import { SituApp } from "./SituApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
