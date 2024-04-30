fef6259ad56b61e8b43aeccb5203b861acee8920
senaempresalagranja <sena.empresa2012@gmail.com>
2024-04-30 04:47:37 -0500

    estructura del proyecto

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { SituApp } from "./SituApp";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SituApp />
-  </React.StrictMode>
-);
